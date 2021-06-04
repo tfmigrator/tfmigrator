79fadb453ca3eac2dd07711221a1c6a458b6a57a
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2021-06-04 10:07:20 +0900

    fix: refactor read_state.go

--- tfmigrator/read_state.go
+++ tfmigrator/read_state.go
@@ -13,15 +13,17 @@ import (
 	"time"
 
 	"github.com/Songmu/timeout"
-	"github.com/sirupsen/logrus"
 )
 
-func (ctrl *Controller) tfShow(ctx context.Context, out io.Writer) error {
-	logrus.Info("terraform show -json")
-	cmd := exec.Command(
-		"terraform", "show", "-json")
-	cmd.Stdout = out
-	cmd.Stderr = ctrl.Stderr
+type TFShowOpt struct {
+	Stdout io.Writer
+	Stderr io.Writer
+}
+
+func TFShow(ctx context.Context, opt *TFShowOpt) error {
+	cmd := exec.Command("terraform", "show", "-json")
+	cmd.Stdout = opt.Stdout
+	cmd.Stderr = opt.Stderr
 	tioStateMv := timeout.Timeout{
 		Cmd:      cmd,
 		Duration: 1 * time.Minute,
@@ -36,25 +38,34 @@ func (ctrl *Controller) tfShow(ctx context.Context, out io.Writer) error {
 	return nil
 }
 
-func (ctrl *Controller) readStateFromCmd(ctx context.Context, state *State) error {
-	buf := bytes.Buffer{}
-	if err := ctrl.tfShow(ctx, &buf); err != nil {
-		return err
-	}
-	if err := json.NewDecoder(&buf).Decode(state); err != nil {
-		return fmt.Errorf("parse a state as JSON: %w", err)
-	}
-	return nil
+type ReadStateFromCmdOpt struct {
+	Stdout io.Writer
+	Stderr io.Writer
 }
 
-func (ctrl *Controller) readState(statePath string, state *State) error {
+func ReadStateFromCmd(ctx context.Context, opt *ReadStateFromCmdOpt, state *State) error {
+	buf := &bytes.Buffer{}
+	if err := TFShow(ctx, &TFShowOpt{
+		Stdout: buf,
+		Stderr: opt.Stderr,
+	}); err != nil {
+		return err
+	}
+	return ReadState(buf, state)
+}
+
+func ReadStateFromFile(statePath string, state *State) error {
 	stateFile, err := os.Open(statePath)
 	if err != nil {
 		return fmt.Errorf("open a state file %s: %w", statePath, err)
 	}
 	defer stateFile.Close()
-	if err := json.NewDecoder(stateFile).Decode(state); err != nil {
-		return fmt.Errorf("parse a state file as JSON %s: %w", statePath, err)
+	return ReadState(stateFile, state)
+}
+
+func ReadState(file io.Reader, state *State) error {
+	if err := json.NewDecoder(file).Decode(state); err != nil {
+		return fmt.Errorf("parse a state file as JSON: %w", err)
 	}
 	return nil
 }
