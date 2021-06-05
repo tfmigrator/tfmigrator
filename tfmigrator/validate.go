74e2833aefc30182ca0c5e1684fd85aaa6809ffa
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2021-06-05 12:18:29 +0900

    refactor: merge files

--- tfmigrator/validate.go
+++ tfmigrator/validate.go
@@ -1,7 +1,18 @@
 package tfmigrator
 
 import (
+	"errors"
+
 	"github.com/go-playground/validator/v10"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 )
 
 var validate = validator.New() //nolint:gochecknoglobals
+
+// ValidateResourceName validates a Terraform Resource name.
+func ValidateResourceName(name string) error {
+	if hclsyntax.ValidIdentifier(name) {
+		return nil
+	}
+	return errors.New("invalid resource name: " + name)
+}
