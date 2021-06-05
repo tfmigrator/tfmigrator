f6622429f3dc1d3eb548e890d86a7775fcc32384
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2021-06-05 19:56:05 +0900

    feat: add QuickRun

---
+++ tfmigrator/quick_run.go
@@ -0,0 +1,63 @@
+package tfmigrator
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+// QuickRun provides CLI interface to run tfmigrator quickly.
+func QuickRun(migrator Migrator) {
+	if err := quickRun(migrator); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func quickRun(migrator Migrator) error {
+	ctx := context.Background()
+	logger := &SimpleLogger{}
+
+	var dryRun bool
+	var help bool
+	var logLevel string
+	flag.BoolVar(&dryRun, "dry-run", false, "dry run")
+	flag.BoolVar(&help, "help", false, "show help message")
+	flag.StringVar(&logLevel, "log-level", "info", "log level")
+	flag.Parse()
+	args := flag.Args()
+
+	if help || (len(args) != 0 && args[0] == "help") {
+		fmt.Fprint(os.Stderr, `tfmigrator - Migrate Terraform Configuration and State
+
+Usage
+  tfmigrator help
+  tfmigrator [-help] [-dry-run] [-log-level debug] [Terraform Configuration file path ...]
+
+Example
+
+  $ ls *.tf | xargs tfmigrator -dry-run -log-level debug
+`)
+		return nil
+	}
+
+	if len(args) == 0 {
+		log.Println("no Terraform Configuration file is passed")
+		return nil
+	}
+
+	if err := logger.SetLogLevel(logLevel); err != nil {
+		return err
+	}
+
+	runner := &Runner{
+		Logger:   logger,
+		DryRun:   dryRun,
+		Migrator: migrator,
+	}
+
+	return runner.Run(ctx, &RunOpt{
+		SourceTFFilePaths: args,
+	})
+}
